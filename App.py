06a6456b294b0eb226354a10c6005c337530d630
mtauban <mathieu.tauban@gmail.com>
2017-02-11 10:38:16 +0000

    changed hysteresis

--- App.py
+++ App.py
@@ -4,11 +4,13 @@ import sendrf
 import thuser
 import thstatus 
 MSG=['PF','PO']
-DELTA_UP=0.2
-DELTA_DOWN=0.3
+DELTA_UP=0.0 # Stops the boiler as soon as the temperature reaches the ref
+DELTA_DOWN=0.3 # starts the boiler at Tref-0.3°C
+
 current=th1w.read_temp() 
 desire=thuser.getUser()
 STATUS=thstatus.getStatus()
+# Programmed as a finite state automata 
 if (float(STATUS) == 1):  # Currently On 
     if (current>desire+DELTA_UP):
         # GO TO OFF 
@@ -19,7 +21,9 @@ else:
         # GO TO ON
         STATUS=1
 
-# final status 
+# Output (stored in log.txt when started by CRON)
 print strftime("%Y-%m-%d %H:%M:%S", gmtime()),current,desire,STATUS,MSG[int(STATUS)]
+
+# Sends to the boiler 
 sendrf.rfSend(MSG[int(STATUS)]) 
 thstatus.setStatus(str(STATUS)) 
